8729bb6861b788cc9268d2be569e962d7a521382
ArminasG <arminas.griesius@gmail.com>
2024-05-15 18:20:09 +0300

    Prop children

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void; //onClick
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   //hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -25,6 +27,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
