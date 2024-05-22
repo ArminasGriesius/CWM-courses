2694820f3257e6d95d602a20e0ed970e7e289383
ArminasG <arminas.griesius@gmail.com>
2024-05-22 16:37:30 +0300

    useState hook

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Buttons from "./components/Buttons/Buttons";
 import Like from "./components/Like/Like";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  let count = 0;
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+    count++;
+    console.log(isVisible);
+    console.log("count ===", count);
+  };
+  console.log("count outside ===", count);
   return (
     <div>
+      <button onClick={handleClick}>Show</button>
       {/* <Buttons onClick={() => {}}>My Button</Buttons> */}
-      <Like onClick={() => console.log("clicked")} />
+      {/* <Like onClick={() => console.log("clicked")} /> */}
     </div>
   );
 }
