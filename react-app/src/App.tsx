b47e0c0febe191495749c7827cbe13a27134155a
ArminasG <arminas.griesius@gmail.com>
2024-06-10 19:24:35 +0300

    useState exercise

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,16 +3,14 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushrooms"],
   });
 
   const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Bob" } });
-    console.log(game);
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    console.log(pizza);
   };
   return (
     <div>
