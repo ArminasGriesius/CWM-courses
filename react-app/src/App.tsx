a8ec68614786a0862c36dd3196028407880069af
ArminasG <arminas.griesius@gmail.com>
2024-06-17 19:12:41 +0300

    fortify useState array of objects

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,24 +2,19 @@ import { useState } from "react";
 import ExpandableText from "./components/ExpandableText.jsx/ExpandableText";
 
 function App() {
-  const [tags, setTags] = useState(["a", "b"]);
-  const handleClickAdd = () => {
-    setTags([...tags, "c"]);
-    console.log(tags);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleClick = () => {
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    console.log(bugs);
   };
-  const handleClickRemove = () => {
-    setTags(tags.filter((tag) => tag !== "a"));
-    console.log(tags);
-  };
-  const handleClickUpdate = () => {
-    setTags(tags.map((tag) => (tag === "a" ? (tag = "A") : tag)));
-    console.log(tags);
-  };
+
   return (
     <div>
-      <button onClick={handleClickAdd}>ClickAdd</button>
-      <button onClick={handleClickRemove}>ClickRemove</button>
-      <button onClick={handleClickUpdate}>ClickUpdate</button>
+      <button onClick={handleClick}>ClickMe</button>
     </div>
   );
 }
