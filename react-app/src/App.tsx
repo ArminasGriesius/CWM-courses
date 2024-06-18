c05854d1809f62d00c2e660fc9c9e7907deecfb1
ArminasG <arminas.griesius@gmail.com>
2024-06-18 17:26:25 +0300

    Forms

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,26 +1,12 @@
 import { useState } from "react";
 import ExpandableText from "./components/ExpandableText.jsx/ExpandableText";
 import produce from "immer";
+import Form from "./components/Form";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
-
-  const handleClick = () => {
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );
-    console.log(bugs);
-  };
-
   return (
     <div>
-      <button onClick={handleClick}>ClickMe</button>
+      <Form />
     </div>
   );
 }
