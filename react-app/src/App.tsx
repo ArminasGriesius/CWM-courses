40e5b023abb609fab6e0baed5688d49b36bdbd6b
ArminasG <arminas.griesius@gmail.com>
2024-06-18 23:42:31 +0300

    formExercise box solution

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
-import ExpandableText from "./components/ExpandableText.jsx/ExpandableText";
-import produce from "immer";
-import Form from "./components/Form";
-import FormExercise from "./components/FormExercise";
-
+// import ExpandableText from "./components/ExpandableText.jsx/ExpandableText";
+// import produce from "immer";
+// import Form from "./components/Form";
+// import FormExercise from "./components/FormExercise";
+import FormExerciseSolution from "./components/expence-tracker/components/FormExerciseSolution";
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utility" },
+    { id: 2, description: "bbb", amount: 10, category: "Utility" },
+    { id: 3, description: "ccc", amount: 10, category: "Utility" },
+    { id: 4, description: "ddd", amount: 10, category: "Utility" },
+  ]);
+
   return (
     <div>
       {/* <Form /> */}
-      <FormExercise />
+      {/* <FormExercise /> */}
+      <FormExerciseSolution
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
