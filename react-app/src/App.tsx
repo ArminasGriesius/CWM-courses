d38c6715ac75508f53d6b03b820cb4eb166fd3dc
ArminasG <arminas.griesius@gmail.com>
2024-06-24 17:12:09 +0300

    handling form submission

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,47 +1,15 @@
-import { useState } from "react";
-// import ExpandableText from "./components/ExpandableText.jsx/ExpandableText";
-// import produce from "immer";
-// import Form from "./components/Form";
-// import FormExercise from "./components/FormExercise";
-import FormExerciseSolution from "./components/expence-tracker/components/FormExerciseSolution";
-import ExpenseFilter from "./components/expence-tracker/components/ExpenseFilter";
-import ExpenseForm from "./components/expence-tracker/components/ExpenseForm";
-import categories from "./components/expence-tracker/categories";
+import React, { FormEvent } from "react";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Groceries" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-  ]);
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
-
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
   return (
     <div>
-      {/* <Form /> */}
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-
-      <FormExerciseSolution
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      <form onSubmit={handleSubmit}>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
