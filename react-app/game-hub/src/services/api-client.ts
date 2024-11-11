057430fed1a0f80d544566415514f091ec9d0d49
ArminasG <arminas.griesius@gmail.com>
2024-11-11 23:12:26 +0200

    changed api

--- react-app/game-hub/src/services/api-client.ts
+++ react-app/game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "cdb3d69605034cc08789b6eb8fb88c6c",
+    key: "979cc31b1ad04b21a44d3c21ed07a793",
   },
 });
